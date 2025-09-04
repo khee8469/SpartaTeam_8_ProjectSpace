aa4bd5979ceadec1ff6f8e1c57a2c72341671e54
khee8469 <bcw1992@naver.com>
2025-09-04 15:37:57 +0900

    Item 헤더 파일 생성
    멤버변수 멤버함수 선언

---
+++ SpartaTeam_8/Item.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+
+using namespace std;
+
+class Character;
+
+class Item
+{
+protected:
+	string name;
+
+public:
+	string getName() { return name; }
+	virtual void Use(Character* character) = 0;
+};
